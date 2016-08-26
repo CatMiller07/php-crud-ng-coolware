991db9054d276215d9a7ccb658d2d40ac6bbbf21
Cathy A. Miller <cat.miller07.cm@gmail.com>
2016-08-26 00:18:13 -0400

    first commit

---
+++ dev/README.txt
@@ -0,0 +1,15 @@
+Hi Coder!
+
+Thanks for downloading this source code and supporting codeofaninja.com!
+
+The step by step source code tutorial can be found on this link: https://www.codeofaninja.com/2015/12/angularjs-crud-example-php.html
+
+SQL database file can be found in this "dev" directory.
+
+If you run into issues, you can contact me via my email: mike@codeofaninja.com
+Or, send me a message on our official Facebook page here: https://fb.com/codeofaninja
+
+Kind Regards,
+Mike Dalisay
+Software Developer
+Founder and Author, codeofaninja.com
---
+++ home.php
@@ -0,0 +1,32 @@
+<?php //some code ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+       <title>Coolwares.com</title>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+         <!--Import Materialize Stylesheet-->
+       <link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+       <linke rel="sytlesheet" type"text/css" href="css/style.css">
+	</head>
+<body class="coolbackgrnd">
+  <!----------Page Header ------------>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Login/SignUp</a></li>
+        <li><a href="badges.html">Links</a></li>    
+      </ul>
+    </div>
+  </nav>
+        
+
+  <!------ jQuery ------>
+  <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+  <!------ jQuery: Materialize ------>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
